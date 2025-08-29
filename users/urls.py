91cd2d638185b3f583700c0e4bdb14177f9a5b6f
Darya <dasha-439@yandex.ru>
2025-08-29 16:52:25 +0300

    Эндпоинты закрыты авторизацией, доступна для анонимных пользователей только регистрация и аутентификация

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.permissions import AllowAny
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
@@ -12,8 +13,8 @@ router.register(r"payments", PaymentViewSet)
 
 urlpatterns = [
     path("register/", UserCreateAPIView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="login"),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh"),
 ]
 
 urlpatterns += router.urls
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 
 from users.filters import PaymentFilter
 from users.models import Payment, User
@@ -10,6 +11,7 @@ from users.serializers import PaymentSerializer, UserSerializer
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
         user = serializer.save(is_active = True)
