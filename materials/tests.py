935cbdaf7374371e3bf48a453bf5c8fcfd9a35d8
Darya <dasha-439@yandex.ru>
2025-09-04 23:07:26 +0300

    Еще тесты для курсов

--- materials/tests.py
+++ materials/tests.py
@@ -25,5 +25,53 @@ class CourseTestCase(APITestCase):
            data.get("name"), self.course.name
         )
 
+    def test_course_create(self):
+        url = reverse("materials:course-list")
+        data = {
+            "name": "Курс 2",
+            "description": "Описание курса 2"
+        }
+        response = self.client.post(url, data)
+        self.assertEqual(
+            response.status_code, status.HTTP_201_CREATED
+        )
+        self.assertEqual(
+            Course.objects.all().count(), 2
+        )
+
+    def test_course_update(self):
+        url = reverse("materials:course-detail", args=(self.course.pk,))
+        data = {
+            "name": "Курс 1 изм "
+        }
+        response = self.client.patch(url, data)
+        data = response.json()
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+        self.assertEqual(
+           data.get("name"), "Курс 1 изм"
+        )
+
+    def test_course_delete(self):
+        url = reverse("materials:course-detail", args=(self.course.pk,))
+        response = self.client.delete(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_204_NO_CONTENT
+        )
+        self.assertEqual(
+           Course.objects.all().count(), 0
+        )
+
+    def test_course_list(self):
+        url = reverse("materials:course-list")
+        response = self.client.get(url)
+        self.assertEqual(
+            response.status_code, status.HTTP_200_OK
+        )
+
+
+
+
 
 
