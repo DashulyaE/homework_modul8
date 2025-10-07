5452bfd0ecf29edcdfce6723b7d313b5584bf708
Darya <dasha-439@yandex.ru>
2025-10-07 22:12:35 +0300

    Изменения16

--- config/settings.py
+++ config/settings.py
@@ -156,11 +156,3 @@ EMAIL_USE_SSL = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-
-if "test" in sys.argv:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
